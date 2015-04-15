fd546d2137970603ac6a4741077a591a97ba07a0
Scott Maclure <scott@maclure.com.au>
2015-04-15 15:19:23 +1000

    Cleaned out old code.

---
+++ gulpfile.js
@@ -0,0 +1,247 @@
+var gulp = require('gulp'),
+	del = require('del'),
+	runSequence = require('run-sequence'),
+	mergeStream = require('merge-stream'),
+	fs = require('fs'),
+	nodePath = require('path'),
+
+	plugins = require('gulp-load-plugins')(),
+
+	testSuite = require('./tasks/testSuite'),
+	ractiveParse = require('./tasks/ractiveParse'),
+	ractiveConcatComponents = require('./tasks/ractiveConcatComponents'),
+	renderDocumentation = require('./tasks/renderDocumentation'),
+	concatManifests = require('./tasks/concatManifests'),
+	gulpWing = require('./tasks/gulpWing'),
+	jshintFailReporter = require('./tasks/jshintFailReporter');
+
+gulp.task('connect', function () {
+	plugins.connect.server({
+		root: 'public',
+		livereload: true,
+		port: 9080
+	});
+});
+
+gulp.task('html', function () {
+	return gulp.src('./public/*.html')
+		.pipe(plugins.connect.reload());
+});
+
+gulp.task('copy-vendors', function () {
+
+	return mergeStream(
+
+		gulp.src([
+			'./node_modules/ractive/ractive.js',
+			'./node_modules/ractive/ractive.min.js',
+			'./node_modules/ractive/ractive.min.js.map',
+			'./node_modules/jquery/dist/jquery.min.js',
+			'./node_modules/jquery/dist/jquery.min.map',
+			'./node_modules/lodash/lodash.min.js',
+			'./node_modules/superagent/superagent.js',
+			'./node_modules/page/page.js',
+			'./src/route.js'
+		])
+		.pipe(gulp.dest('./public/js')),
+
+		gulp.src([
+			'node_modules/zurb-foundation-5/doc/assets/img/images/**/*'
+		])
+		.pipe(gulp.dest('public/images/'))
+
+	);
+
+});
+
+gulp.task('copy-use-cases', function () {
+	return gulp.src([
+		'./src/components/**/use-cases/*.json'
+	])
+		.pipe(plugins.rename(function (path) {
+			// Get rid of the extra "use-cases" folder for the destination.
+			path.dirname = path.dirname.split(nodePath.sep)[0];
+		}))
+		.pipe(gulp.dest('public/use-cases/'));
+});
+
+gulp.task('clean', function (callback) {
+	del([
+		'public/**/*'
+	], callback);
+});
+
+gulp.task('build-sass', function () {
+
+	return mergeStream(
+
+		gulp.src('./src/**/*.scss')
+			.pipe(plugins.sass())
+			.pipe(plugins.concat('components.css'))
+			.pipe(gulp.dest('./public/css')),
+
+		gulp.src('./node_modules/zurb-foundation-5/scss/*.scss')
+			.pipe(plugins.sass())
+			.pipe(gulp.dest('./public/css/foundation'))
+
+	);
+
+});
+
+gulp.task('ractive-build-templates', function () {
+	return gulp.src('./src/components/**/*.hbs')
+		.pipe(ractiveParse({
+			'prefix': 'RactiveF'
+		}))
+		.pipe(plugins.concat('templates.js'))
+		.pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('ractive-build-components', function () {
+	return gulp.src([
+			'./src/components/**/*.js',
+			'!./src/components/**/*.steps.js'
+		])
+		.pipe(ractiveConcatComponents({
+			'prefix': 'RactiveF'
+		}))
+		.pipe(plugins.concat('components.js'))
+		.pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('build-documentation', function () {
+
+	return mergeStream(
+
+		// Component docs page.
+		gulp.src('./src/components/**/manifest.json')
+		.pipe(concatManifests('manifest-all.js'))
+		.pipe(gulp.dest('./public/'))
+		.pipe(renderDocumentation())
+		.pipe(plugins.concat('docs.html'))
+		.pipe(plugins.header(fs.readFileSync('./src/header.html')))
+		.pipe(plugins.footer(fs.readFileSync('./src/footer.html')))
+		.pipe(gulp.dest('./public/')),
+
+		// Documentation pages.
+		gulp.src([ './src/pages/*.html' ])
+		.pipe(plugins.header(fs.readFileSync('./src/header.html')))
+		.pipe(plugins.footer(fs.readFileSync('./src/footer.html')))
+		.pipe(gulp.dest('./public/')),
+
+		// Test runner while we're at it.
+		gulp.src('./src/testRunner.html')
+			.pipe(gulp.dest('./public/'))
+
+	);
+
+});
+
+gulp.task('concat-app', function () {
+	var files = [
+		'./src/ractivef.base.js',
+		'./public/js/templates.js',
+		'./public/js/components.js'
+	];
+	return gulp.src(files)
+		.pipe(plugins.concat('ractivef-base.js'))
+		.pipe(gulp.dest('./public/js/'))
+		.pipe(plugins.footer(fs.readFileSync('./src/ractivef.initializer.js')))
+		.pipe(plugins.concat('ractivef.js'))
+		.pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('concat-app-amd', function () {
+	return gulp.src('./public/js/ractivef-base.js')
+		.pipe(plugins.wrap({ src: './src/ractivef-amd.js'}))
+		.pipe(plugins.concat('ractivef-amd.js'))
+		.pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('concat-app-commonjs', function () {
+	return gulp.src('./public/js/ractivef-base.js')
+		.pipe(plugins.wrap({ src: './src/ractivef-cjs.js'}))
+		.pipe(plugins.concat('ractivef-cjs.js'))
+		.pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('wing', function (callback) {
+	gulpWing();
+	callback();
+});
+
+gulp.task('build', ['clean', 'jshint'], function (callback) {
+	runSequence([
+		'build-sass',
+		'ractive-build-templates',
+		'ractive-build-components',
+		'build-documentation'
+	], [
+		'copy-vendors',
+		'copy-use-cases',
+		'concat-app'
+	], [
+		'concat-app-amd',
+		'concat-app-commonjs'
+	], callback);
+});
+
+gulp.task('dist', ['build'], function () {
+	return gulp.src([
+		'public/js/ractivef-amd.js',
+		'public/js/ractivef-base.js',
+		'public/js/ractivef-cjs.js',
+		'public/css/components.css'
+	]).pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+	var self = this;
+	plugins.watch([
+		'src/*.html',
+		'src/pages/*.html',
+		'src/**.*.json',
+		'src/**/*.hbs',
+		'src/**/*.md',
+		'src/**/*.js',
+		'src/**/*.scss'
+	], function () {
+		runSequence('build', 'html', function (err) {
+			self.emit('end');
+		});
+	});
+
+});
+
+gulp.task('cucumber', function(callback) {
+	return gulp
+		.src('./src/components/**/*.feature')
+		.pipe(
+			testSuite(
+				{ steps: './src/components/**/*.steps.js' }
+			).on('error', function () {
+				// Prevent stack trace
+				this.emit('end');
+			})
+		);
+});
+
+gulp.task('test', function (callback) {
+	runSequence('build', 'connect', 'cucumber', function (err) {
+   		process.exit(err ? 1 : 0);
+    });
+});
+
+gulp.task('jshint', function (callback) {
+	return gulp.src('./src/**/*.js')
+		.pipe(plugins.jshint('./.jshintrc'))
+		.pipe(plugins.jshint.reporter('jshint-stylish'))
+		.pipe(jshintFailReporter());
+});
+
+gulp.task('default', function () {
+	var self = this;
+	runSequence('jshint', 'build',  'connect', 'watch', function (err) {
+		self.emit('end');
+	});
+});
---
+++ tasks/gulpWing.js
@@ -0,0 +1,87 @@
+/*global module*/
+/**
+ * Gulp task to create components, including dynamic content.
+ * Very simple.
+ *
+ * TODO Consider async code style, but really, it takes 1.16ms to run this code on my laptop.
+ */
+
+var _ = require('lodash'),
+	fs = require('fs'),
+	gutil = require('gulp-util'),
+	path = require('path'),
+	walk = require('walk');
+
+const PLUGIN_NAME = 'gulp-wing';
+
+const PLUGIN_ERROR_NAME_PARAM = 'wing needs --name param to work.';
+const PLUGIN_ERROR_NAME_LOWERCASE = 'Component name must be all lowercase alphanumeric, ' +
+	'and begin with "ux-". Example: ux-';
+
+const GULP_WING_SOURCE_PATH_PREFIX = './tasks/gulpWingFiles';
+const GULP_WING_TARGET_PATH_PREFIX = './src/components/';
+const GULP_WING_PLACEHOLDER = 'wingComponent';
+
+var lowerCaseOnly = new RegExp('^[a-z-]+$', 'g');
+
+function processFileAndSave(name, destPath, root, sourceFile) {
+
+	var source = [root, '/', sourceFile],
+		regex = new RegExp(GULP_WING_PLACEHOLDER, 'g'),
+		targetFile = sourceFile.replace(regex, name),
+		fc = fs.readFileSync(source.join(''), 'UTF-8');
+
+	fc = fc.replace(regex, name);
+
+	var dir = root.replace(GULP_WING_SOURCE_PATH_PREFIX, '');
+	if (dir) {
+		dir += '/';
+	}
+
+	fs.writeFileSync(destPath + dir + targetFile, fc);
+}
+
+module.exports = function () {
+
+	var option = require('node-getopt-long').options([
+		['name|n=s', {
+			description: 'The name of the component to create',
+			paramName: 'ux-...',
+			test: function (value) {
+				// Check for ux-*, including nothing at all to error out on.
+				if (value.length < 4) {
+					throw new gutil.PluginError(PLUGIN_NAME, PLUGIN_ERROR_NAME_PARAM);
+				}
+
+				// Names of components MUST be lowercase, and start with 'ux-'.
+				if (!lowerCaseOnly.test(value) || 'ux-' !== value.substr(0, 3)) {
+					throw new gutil.PluginError(PLUGIN_NAME, PLUGIN_ERROR_NAME_LOWERCASE + value.toLowerCase());
+				}
+
+				return value;
+			}
+		}]
+	], {
+		name: 'gulp wing'
+	});
+
+	if (!option.name) {
+		throw new gutil.PluginError(PLUGIN_NAME, PLUGIN_ERROR_NAME_PARAM);
+	}
+
+	var destPath = GULP_WING_TARGET_PATH_PREFIX + option.name + '/';
+	fs.mkdirSync(destPath);
+	// Create deep folder
+	fs.mkdirSync(destPath + 'use-cases/');
+
+
+	// Recurse through all files and folders in source.
+	var walker = walk.walk(GULP_WING_SOURCE_PATH_PREFIX, {followLinks: false});
+	walker.on('file', function (root, stat, next) {
+		processFileAndSave(option.name, destPath, root, stat.name);
+		next();
+	});
+
+	return true;
+
+};
