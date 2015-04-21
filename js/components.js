1ed4df3d5bb59f0eacc1a689be0d311c0b62f388
Scott Maclure <scott@maclure.com.au>
2015-04-21 19:12:33 +1000

    gh-pages publish

--- js/components.js
+++ js/components.js
@@ -214,6 +214,51 @@ RactiveF.components['ux-li'] = Ractive.extend({
 	template: RactiveF.templates['ux-li']
 });
 
+RactiveF.components['ux-off-canvas'] = Ractive.extend({
+
+	template: RactiveF.templates['ux-off-canvas'],
+
+	data: function () {
+		return {
+			expandedState: '',
+			leftItems: [],
+			rightItems: [],
+			mainContent: ''
+		};
+	},
+
+	computed: {
+
+		/**
+		 * @returns {string} CSS class: left = move-right or right = move-left.
+		 */
+		getExpandedClass: function () {
+
+			switch (this.get('expandedState')) {
+				case 'left':
+					return 'move-right';
+				case 'right':
+					return 'move-left';
+			}
+
+			// Default is empty string for no css class.
+			return '';
+
+		}
+
+	},
+
+	oninit: function () {
+
+		// You can expand from left or right, or none. Can't do both at the same time.
+		this.on('toggleMenu', function (event, direction) {
+			this.set('expandedState', direction);
+		});
+
+	}
+
+});
+
 RactiveF.components['ux-panel'] = Ractive.extend({
 	template: RactiveF.templates['ux-panel']
 });
