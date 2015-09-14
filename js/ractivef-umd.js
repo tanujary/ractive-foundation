c60f6d61f127d3ec7ecb6a767bc3ed86f772fc1d
Scott Maclure <scott@maclure.com.au>
2015-09-14 21:42:14 +1000

    gh-pages publish

--- js/ractivef-umd.js
+++ js/ractivef-umd.js
@@ -1,6 +1,6 @@
 /**
  * ractive-foundation - Ractive components for Foundation 5
- * @version 0.1.1
+ * @version 0.2.0
  * @link https://github.com/ractive-foundation/ractive-foundation
  * @license MIT
  */
